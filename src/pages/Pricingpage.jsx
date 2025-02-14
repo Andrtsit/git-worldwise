628edbcc4fceaaad6435a860a16bda473bffe21f
Andrtsit <andrtsit@gmail.com>
2025-02-14 11:52:48 +0200

    transfer files commit

---
+++ src/pages/Pricingpage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pricingpage() {
+  return (
+    <div>
+      <PageNav />
+      Pricinpage
+    </div>
+  );
+}
+
+export default Pricingpage;
